3532d59bcc802adb17073faae7f5b6ae2f80ce5a
Ikechukwu Okoro <ikeshegs@gmail.com>
2023-01-02 14:33:47 +0100

    add the user model

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb://mongo:27017/jp-crm-db';
 const options = {
-  autoIndex: false,
-  maxPoolSize: 10, 
-  serverSelectionTimeoutMS: 5000, 
-  socketTimeoutMS: 45000,
-  family: 4
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // IPV4
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 }
 
 mongoose.connect(uri, options);
--- server.js
+++ server.js
@@ -7,14 +7,11 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 async function startServer () {
-  // Database connection should be here
   db.on("error", console.error.bind(console, "connection error: "));
   db.once("open", function () {
-    console.log("DB Connected successfully");
-  });
-
-  server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
   });
 }
 
